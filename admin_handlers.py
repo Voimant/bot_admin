07546881aec003ae31b38b0751bbf447976bbc8b
Voimant <voimant007@gmail.com>
2024-03-29 11:48:18 +0500

    update_chat

--- admin_handlers.py
+++ admin_handlers.py
@@ -36,8 +36,6 @@ async def get_open_chat(mess: Message, command: CommandObject):
             await asyncio.sleep(20)
 
 
-
-
 @router.message(Command('add_group'))
 async def get_add_group(mess: Message):
     """Добавление группы в отслеживаемые у бота"""
@@ -71,6 +69,7 @@ async def get_add(mess: Message, command: CommandObject):
     else:
         pass
 
+
 # @router.message(Command('edit'))
 # async def bot_settings(mess: Message):
 #     if mess.chat.type == 'private':
@@ -82,72 +81,67 @@ async def get_add(mess: Message, command: CommandObject):
 #         await new_msg.delete()
 
 
-#@router.message(F.content_type == types.ContentType.NEW_CHAT_MEMBERS)ChatMemberUpdatedFilter
+# @router.message(F.content_type == types.ContentType.NEW_CHAT_MEMBERS)ChatMemberUpdatedFilter  .new_chat_members
 @router.chat_member(ChatMemberUpdatedFilter(JOIN_TRANSITION))
 async def new_members(mem: ChatMember):
     """При событии добавления нового учасника, добавляем в базу
     -- Добавляет в базу нового участника, ставит inv = 0
     -- + 1 человек, узеру которойй добавил пользователя"""
-    # print(mess.from_user.id)
-    # my_db = db_select_users()
-    # if str(mess.from_user.id) in my_db:
-    #     pass
-    # else:
-    #     inv = 0
-    #     username = mess.from_user.username
-    #     invite_user(mess.from_user.id, inv, username)
-    # for user in mess.new_chat_members:
-    #     inviter = None
-    #     if user.id:
-    #         inviter = user.id
-    #     elif user.sender_chat:
-    #         inviter = user.sender_chat.user.id
-    #     print(f"Пользователь {user.full_name} был приглашен {inviter}\n"
-    #           f"Пригласивший: {mess.from_user.id}\n")
-    #     inv = 0
-    #     for y_user in mess.new_chat_members:
-    #         you_username = y_user.username
-    #         invite_user(inviter, inv, you_username)
-    #         db_update_invate(mess.from_user.id)
-    #         conn.commit()
+    print(mem.from_user.id)
+    my_db = db_select_users()
+    if str(mem.from_user.id) in my_db:
+        pass
+    else:
+        inv = 0
+        username = mem.from_user.username
+        invite_user(mem.from_user.id, inv, username)
+    invited = mem.new_chat_member.user.id
+    inviter = mem.from_user.id
+    print(f"Пользователь {mem.new_chat_member.user.username} был приглашен {invited}\n"
+          f"Пригласивший: {inviter}\n")
+    inv = 0
+    you_username = mem.new_chat_member.user.username
+    invite_user(invited, inv, you_username)
+    db_update_invate(mem.from_user.id)
+    conn.commit()
     print(mem)
 
 
-# @router.message()
-# async def members(mess: Message):
-#     """Функция не пропускает текстовое сообщения пока не будет 10 приглашенных участников"""
-#
-#     if int(mess.chat.id) < 0:
-#         user_id = mess.from_user.id
-#         group_id = mess.chat.id
-#         username = mess.from_user.username
-#         save_user(str(user_id), username)
-#         try:
-#             threshold = db_group_invites(str(group_id))
-#             invites = you_invite(user_id)
-#             print(threshold[0])
-#             print(mess.from_user.id)
-#             for inv in invites:
-#                 print(inv)
-#                 if inv < threshold[0] and int(mess.from_user.id) not in [423947942, 5805441535] and mess.from_user.is_bot is False:
-#                     await mess.delete()
-#                     button_1 = [[InlineKeyboardButton(text='Опубликовать объявления', url='https://t.me/OMKS312_bot')]]
-#                     markup = InlineKeyboardMarkup(inline_keyboard=button_1)
-#                     new_msg = await mess.answer(f'{mess.from_user.username} Пожалуйста добавьте {str(threshold[0])} пользователей,'
-#                                                 f' после этого сможете публиковать объявления. Для публикации объявлений без добавления участников\n'
-#                                                 f'Или напишите администратору https://t.me/kronus16 '
-#                                                 f'нажмите кнопку ниже\n\n'
-#                                                 f' НА ДАННЫЙ МОМЕНТ ВЫ ДОБАВИЛИ: 👉🏻 {inv} пользователей\n'
-#                                                 f'А если вам понравился бот и хочешь такого же жми сюда 👉🏻 https://t.me/nbchatbot_bot', reply_markup=markup)
-#                     print(mess.from_user.id)
-#                     await asyncio.sleep(30)
-#                     try:
-#                         await new_msg.delete()
-#                     except Exception as e:
-#                         print(e)
-#                 elif inv >= threshold[0] or mess.from_user.id in [423947942, 5805441535]:
-#                     pass
-#         except TypeError:
-#             await mess.answer('Группа не добавлена в базу, напишите разработчикам что бы начать пользоваться ботом\n https://t.me/nbchatbot_bot')
-#     else:
-#         await mess.answer('Чат бот предназначен для использования в группах, обратитесь к разработчикам или администратору')
+@router.message()
+async def members(mess: Message):
+    """Функция не пропускает текстовое сообщения пока не будет 10 приглашенных участников"""
+
+    if int(mess.chat.id) < 0:
+        user_id = mess.from_user.id
+        group_id = mess.chat.id
+        username = mess.from_user.username
+        save_user(str(user_id), username)
+        try:
+            threshold = db_group_invites(str(group_id))
+            invites = you_invite(user_id)
+            print(threshold[0])
+            print(mess.from_user.id)
+            for inv in invites:
+                print(inv)
+                if inv < threshold[0] and int(mess.from_user.id) not in [423947942, 5805441535] and mess.from_user.is_bot is False:
+                    await mess.delete()
+                    button_1 = [[InlineKeyboardButton(text='Опубликовать объявления', url='https://t.me/OMKS312_bot')]]
+                    markup = InlineKeyboardMarkup(inline_keyboard=button_1)
+                    new_msg = await mess.answer(f'{mess.from_user.username} Пожалуйста добавьте {str(threshold[0])} пользователей,'
+                                                f' после этого сможете публиковать объявления. Для публикации объявлений без добавления участников\n'
+                                                f'Или напишите администратору https://t.me/kronus16 '
+                                                f'нажмите кнопку ниже\n\n'
+                                                f' НА ДАННЫЙ МОМЕНТ ВЫ ДОБАВИЛИ: 👉🏻 {inv} пользователей\n'
+                                                f'А если вам понравился бот и хочешь такого же жми сюда 👉🏻 https://t.me/nbchatbot_bot', reply_markup=markup)
+                    print(mess.from_user.id)
+                    await asyncio.sleep(30)
+                    try:
+                        await new_msg.delete()
+                    except Exception as e:
+                        print(e)
+                elif inv >= threshold[0] or mess.from_user.id in [423947942, 5805441535]:
+                    pass
+        except TypeError:
+            await mess.answer('Группа не добавлена в базу, напишите разработчикам что бы начать пользоваться ботом\n https://t.me/nbchatbot_bot')
+    else:
+        await mess.answer('Чат бот предназначен для использования в группах, обратитесь к разработчикам или администратору')
